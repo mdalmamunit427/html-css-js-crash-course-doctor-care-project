feb0083410ea009898b1062c3046c0afd1cbb75b
mdalmamunit427 <mdalmamunit427@gmail.com>
2024-09-02 13:30:37 +0600

    update animation

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,23 @@
 const animation = ScrollReveal({
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    reset: true
-  })
-  animation.reveal('.heroimage, .hero-content, .service-content, .about-section, .contact-content', {delay: 200, origin: 'bottom'});
-  animation.reveal('.stats-section, .contact-form', {delay: 200, origin: 'right'});
-  animation.reveal('.stats-section, , .contact-form', {delay: 200, origin: 'left'});
-  animation.reveal('.contact-info, .footer', {delay: 200, origin: 'top'});
+  distance: '30px',
+  duration: 1500,  
+  delay: 400,
+  reset: true
+});
+
+animation.reveal('.heroimage, .hero-content, .service-content, .about-section, .contact-content', {
+  delay: 200,
+  origin: 'bottom'
+});
+animation.reveal('.stats-section, .contact-form', {
+  delay: 200,
+  origin: 'right'
+});
+animation.reveal('.stats-section, .contact-form', {
+  delay: 200,
+  origin: 'left'
+});
+animation.reveal('.contact-info, .footer', {
+  delay: 200,
+  origin: 'top'
+});
